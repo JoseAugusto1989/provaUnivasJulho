97eccfa6f994d844e40ffee9db659e932090316b
Jose Augusto de Oliveira Pins <64021880+JoseAugusto1989@users.noreply.github.com>
2022-07-04 20:23:21 -0300

    third commit

--- src/app/components/library/library.component.ts
+++ src/app/components/library/library.component.ts
@@ -1,4 +1,7 @@
+import { NgForm } from '@angular/forms';
+import { LibraryService } from './library.service';
 import { Component, OnInit } from '@angular/core';
+import {Library} from '../library/library'
 
 @Component({
   selector: 'app-library',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryComponent implements OnInit {
 
-  constructor() { }
+  libraryList: Library [] = []
+  newLibrary: Library = {} as Library
+
+  constructor(private service: LibraryService) { }
 
   ngOnInit(): void {
+  }
+
+  saveData(form: NgForm) {
+    this.service.getList(this.newLibrary).subscribe(item => {
+      form.resetForm()
+      this.newLibrary = {} as Library
+      this.loadData()
+    })
+  }
+
+  loadData() {
+    this.service.getList().subscribe(items => {
+      this.libraryList = items
+    })
   }
 
 }
